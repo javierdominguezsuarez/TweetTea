e8263fda16223fc291102ccad6beaee0bdd5ff15
javier dominguez <francisco.dominguez111@alu.ulpgc.es>
2021-01-28 02:08:29 +0000

    Advancing-Rest

--- tweetme2/tweets/views.py
+++ tweetme2/tweets/views.py
@@ -8,9 +8,13 @@ from rest_framework.response import Response
 from .models import Tweet
 from .forms import TweetForm
 from .serializers import TweetSerializer, serializers
+
 def home_view(request, *args, **kwargs):
     #return HttpResponse("<h1>Hellow World</h1>")
     return render (request,"pages/home.html", context = {}, status = 200)
+
+#Using old django
+
 
 def tweet_detail_view_old(request, tweet_id, *args , **kwargs):
     """
@@ -31,6 +35,33 @@ def tweet_detail_view_old(request, tweet_id, *args , **kwargs):
         status = 404
     return JsonResponse(data, status = status)
 
+def tweet_list_view_old (request, *args, **kwargs):
+    """
+    REST API VIEW 
+    Consume by JavaScript  
+    return json data 
+    """
+    tweet_list = Tweet.objects.all()
+    data_list = [{"id": t.id,"content": t.content, "likes": 0} for t in tweet_list]
+    data = {
+        "isUser": False ,
+        "response": data_list
+    }
+    return JsonResponse(data_list,safe= False)
+
+def tweet_create_view_old (request, *args, **kwargs):
+    """
+    REST API CREATE VIEW 
+    """
+    form = TweetForm(request.POST or None)
+    if form.is_valid():
+        obj = form.save(commit = False)
+        obj.save()
+        form = TweetForm()
+    return render(request, 'components/forms.html', context = {"form" : form})
+
+# Using django rest framework
+
 @api_view(['GET'])
 def tweet_detail_view (request,tweet_id,  *args, **kwargs):
     obj = Tweet.objects.get(id = tweet_id)
@@ -46,19 +77,7 @@ def tweet_list_view (request, *args, **kwargs):
     serializer = TweetSerializer(tweet_list, many = True)
     return Response(serializer.data, status = 200)
 
-def tweet_list_view_old (request, *args, **kwargs):
-    """
-    REST API VIEW 
-    Consume by JavaScript  
-    return json data 
-    """
-    tweet_list = Tweet.objects.all()
-    data_list = [{"id": t.id,"content": t.content, "likes": 0} for t in tweet_list]
-    data = {
-        "isUser": False ,
-        "response": data_list
-    }
-    return JsonResponse(data_list,safe= False)
+
 
 @api_view(['POST'])
 def tweet_create_view (request,*args,**kwargs):
@@ -71,16 +90,3 @@ def tweet_create_view (request,*args,**kwargs):
         return Response(serializer.data, status = 201)  
 
     return Response({}, status = 400)
-
-
-
-def tweet_create_view_old (request, *args, **kwargs):
-    """
-    REST API CREATE VIEW 
-    """
-    form = TweetForm(request.POST or None)
-    if form.is_valid():
-        obj = form.save(commit = False)
-        obj.save()
-        form = TweetForm()
-    return render(request, 'components/forms.html', context = {"form" : form})
