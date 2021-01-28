72038f002ed04f2e064b81b742e81ae5ca8f0d25
pepe <jrpenaseco@gmail.com>
2021-01-28 01:51:01 +0000

    hello world

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,19 @@ import { AiFillPicture } from "react-icons/ai";
 import WriteTweet from './Components/WriteTweet';
 import Tweet from './Components/Tweet';
 import Pallete from './Components/Pallete';
+import useApiCallback from './customHooks/useApiCallback'
+import axios from 'axios';
+import { useEffect } from 'react';
 function App() {
+
+
+  const request = useApiCallback(() => axios.get('http://127.0.0.1:8000/tweets/'), (data) => console.log(data))
+
+  useEffect(() => {
+    request.request()
+  }, [])
+
+
   return (
     <div className="App" style={{backgroundColor : Pallete.primary}}>
       <div className="container p-4">
