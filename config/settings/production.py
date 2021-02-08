24c5cb44dd025a4b0692acbfcdc159306a2b499c
javier dominguez <francisco.dominguez111@alu.ulpgc.es>
2021-02-08 23:28:54 +0000

    r

--- config/settings/production.py
+++ config/settings/production.py
@@ -97,6 +97,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'config.wsgi.application'
 
 
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
