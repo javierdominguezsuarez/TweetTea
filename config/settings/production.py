ba491390838f985949d56954179a4c82b6b25723
javier dominguez <francisco.dominguez111@alu.ulpgc.es>
2021-02-08 23:22:03 +0000

    r

--- config/settings/production.py
+++ config/settings/production.py
@@ -154,4 +154,11 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR , "static"),
+    #'/var/www/static/',
+]
+STATIC_ROOT = os.path.join((BASE_DIR),"static_cdn")
+MEDIA_ROOT = os.path.join((BASE_DIR),"media_cdn")
