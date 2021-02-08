954bda798844be4620d1b3be8969141f158b4c94
javier dominguez <francisco.dominguez111@alu.ulpgc.es>
2021-02-08 23:04:33 +0000

    hola

--- config/settings/production.py
+++ config/settings/production.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
-print(" Prod settings")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(os.path.abspath(__file__)).resolve().parent.parent
 MAX_LENGTH = 240
