7f9d666ec0c5d7d4006dbbae0b8dc998a8c09bb2
javier dominguez <francisco.dominguez111@alu.ulpgc.es>
2021-02-08 23:48:08 +0000

    r

--- config/settings/production.py
+++ config/settings/production.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'w__d%1v(-ya7_&0d*r^=ty4j!(ps5hao!#c@la6_og-labqzzz'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 LOGIN_URL = "/login"
 ALLOWED_HOSTS = [".herokuapp.com",".herokuapps."]
 
@@ -151,41 +151,3 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join((BASE_DIR),"static_cdn")
 MEDIA_ROOT = os.path.join((BASE_DIR),"media_cdn")
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
-                       'pathname=%(pathname)s lineno=%(lineno)s '
-                       'funcname=%(funcName)s %(message)s'),
-            'datefmt': '%Y-%m-%d %H:%M:%S'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        }
-    },
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'logging.NullHandler',
-        },
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'django.request': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': False,
-        },
-    }
-}
