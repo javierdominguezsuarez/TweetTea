f47415499523a18aa2b138129cce1e9a200153ec
Francisco Javier Dominguez Suarez <56936193+javierdominguezsuarez@users.noreply.github.com>
2021-02-10 12:30:02 +0000

    Delete tweetme2 directory

--- tweetme2/utils/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class AppBaseModel (models.Model):
-    pub = models.DateField("Date of publication",auto_now_add=True)
-    hour = models.TimeField("Time of publication", auto_now_add=True)
-    class Meta :
-        abstract = True
-        ordering = ['-pub','-hour']
